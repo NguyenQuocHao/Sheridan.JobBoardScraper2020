ed2dcb7e6672d3af5982dc619cddcdaa306fa861
Hao Nguyen <nguyenquochao2000@gmail.com>
2020-12-27 01:41:05 -0500

    Refactored so that it works with 11 later pages. To-do: The program recognizes when to stop.

--- main.py
+++ main.py
@@ -53,16 +53,16 @@ sheet.cell(row=1, column=12).value = "Salary"
 sheet.cell(row=1, column=13).value = "Compensation"
 sheet.cell(row=1, column=14).value = "Description"
 
-# Get job board table content
-soup = BeautifulSoup(browser.page_source, 'lxml')  # load page content
-My_table = soup.find('table', {'class': 'table table-bordered'})
-findTd = My_table.findAll('td')  # scan through table, and store all 'td' elems into an array
-
 count = 0  # Number of moves (cell to cell)
 countX = 1  # Column
 countY = 2  # Row
 
+# Get job board table content
+
 def collect_job_postings():
+    soup = BeautifulSoup(browser.page_source, 'lxml')  # load page content
+    My_table = soup.find('table', {'class': 'table table-bordered'})
+    findTd = My_table.findAll('td')  # scan through table, and store all 'td' elems into an array
     for i in findTd:
         global count
         global countX
@@ -100,81 +100,11 @@ def collect_job_postings():
 
 # first page
 collect_job_postings()
-# for i in findTd:
-#      # copy salary & compensation
-#      if count % 11 == 0:  # execute if first cell
-#          sheet.cell(row=countY, column=countX).value = job_id = i.text.replace('\n', '').strip()
-#          locator = browser.find_element_by_link_text(job_id)
-#          locator.click()
-#          soup = BeautifulSoup(browser.page_source, 'lxml')  # update buffered link to current link
-#          My_Salary = soup.find('div', {'id': 'jd9'})
-#          My_Compensation = soup.find('div', {'id': 'jd10'})
-#          My_Description = soup.find('div', {'id': 'jobd1'})
-#          if My_Salary is not None:
-#              findSalary = My_Salary.findAll('p')[0].text.replace('\n', '').strip()
-#              sheet.cell(row=countY, column=12).value = findSalary
-#          if My_Compensation is not None:
-#              findCompensation = My_Compensation.findAll('p')[0].text.replace('\n', '').strip()
-#              sheet.cell(row=countY, column=13).value = findCompensation
-#          if My_Description is not None:
-#              findDescription = My_Description.findAll('p')[0].text.replace('\n', '').strip()
-#              sheet.cell(row=countY, column=14).value = findDescription
-#          browser.back()
-#
-#      elif count % 11 != 0:  # execute if not first cell
-#          sheet.cell(row=countY, column=countX).value = i.text.replace('\n', '').strip()
-#          # Check if the last cell
-#          if count % 11 == 10:
-#              countY += 1
-#              countX = 0
-#
-#      # Increment anyways
-#      countX += 1
-#      count += 1
-
 # later pages.
-# for page in range(0, 17):
-#     linkElem = browser.find_element_by_link_text('Next')
-#     linkElem.click()
-#     soup = BeautifulSoup(browser.page_source, 'lxml')  # update buffered link to current link
-#     My_table = soup.find('table', {'class': 'table table-bordered'})
-#     findTd = My_table.findAll('td')
-#     for i in findTd:
-#         # copy table data into excel file
-#         if count % 11 != 0:
-#             sheet.cell(row=countY, column=countX).value = i.text.replace('\n', '').strip()
-#             countX += 1
-#         else:
-#             countY += 1
-#             countX = 1
-#             sheet.cell(row=countY, column=countX).value = i.text.replace('\n', '').strip()
-#             countX += 1
-#
-#         # copy salary & compensation
-#         if count % 11 == 0:
-#             job_id = i.text.replace('\n', '').strip()
-#             print('                ' + job_id)
-#             locator = browser.find_element_by_link_text(job_id)
-#             locator.click()
-#             soup = BeautifulSoup(browser.page_source, 'lxml')
-#             My_Salary = soup.find('div', {'id': 'jd9'})
-#             My_Compensation = soup.find('div', {'id': 'jd10'})
-#             My_Description = soup.find('div', {'id': 'jobd1'})
-#             if My_Salary is not None:
-#                 findSalary = My_Salary.findAll('p')[0].text.replace('\n', '').strip()
-#                 print("sal :" + findSalary)
-#                 sheet.cell(row=countY, column=12).value = findSalary
-#             if My_Compensation is not None:
-#                 findCompensation = My_Compensation.findAll('p')[0].text.replace('\n', '').strip()
-#                 print("pen :" + findCompensation)
-#                 sheet.cell(row=countY, column=13).value = findCompensation
-#             # Get Description
-#             if My_Description is not None:
-#                 findDescription = My_Description.findAll('p')[0].text  # .replace('\n', '').strip()
-#                 print("des :" + findDescription)
-#                 sheet.cell(row=countY, column=14).value = findDescription
-#             browser.back()
-#         count += 1
+for page in range(0, 11):
+    linkElem = browser.find_element_by_link_text('Next')
+    linkElem.click()
+    collect_job_postings()
 
 # Write (save) the excel file into the drive
 wb.save('sheridan_job_board_fall_2020.xlsx')
